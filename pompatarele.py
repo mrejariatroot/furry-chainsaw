4d9ae7375759cca37eb671b8974d97320119672a
XGamingbg <48446414+XGamingbg@users.noreply.github.com>
2019-03-16 17:30:15 +0200

    pomparatarele updated

--- pompatarele.py
+++ pompatarele.py
@@ -8,19 +8,19 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
 
-def motor_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  #vkliuchvane na pompata
+def rele2_on():
+    GPIO.output(20, GPIO.HIGH)  #vkliuchvane na pompata
 
 
-def motor_off(pin):
-    GPIO.output(pin, GPIO.LOW) #izkliuchvane na pompata
+def rele2_off():
+    GPIO.output(20, GPIO.LOW) #izkliuchvane na pompata
 
 
 if __name__ == '__main__':
     try:
-        motor_on(channel)
+        rele2_on(channel)
         time.sleep(1)
-        motor_off(channel)
+        rele2_off(channel)
         time.sleep(1)
         GPIO.cleanup()
     except KeyboardInterrupt:
