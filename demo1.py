4598bca7e07e1cf0bff04039b645ecf08f402e01
XGamingbg <48446414+XGamingbg@users.noreply.github.com>
2019-03-16 12:18:03 +0200

    updated with rele1 & rele2 with infinity voice

--- demo1.py
+++ demo1.py
@@ -1,18 +1,18 @@
 import speech_recognition as sr
 import relay as rl
 import time
+import pompatarele as ml
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-    print(r.recognize_google(audio))
-    if(r.recognize_google(audio)=="make me coffee"):
-        rl.motor_on(21)
-        time.sleep(10)#ne znaem vremeto
-        rl.motor_off()
-        
-            
-        
-        
-
+while True:
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        print(r.recognize_google(audio))
+        if(r.recognize_google(audio)=="make me coffee"):
+            rl.rele1_on(21)
+            time.sleep(10)#ne znaem vremeto
+            rl.rele1_off()
+            ml.rele2_on(20)
+            time.sleep(5)
+            ml.rele2_off()
 
