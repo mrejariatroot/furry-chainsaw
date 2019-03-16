5aefa0c44716d1355333cec34ee3a5f7bd1f4ee9
XGamingbg <48446414+XGamingbg@users.noreply.github.com>
2019-03-16 17:30:44 +0200

    working relay

--- relay.py
+++ relay.py
@@ -1,29 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
-channel = 21
+channel = 16
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.OUT)
 
 
-def motor_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+def rele1_on():
+    GPIO.output(16, GPIO.HIGH)  # Turn motor on
 
 
-def motor_off(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+def rele1_off():
+    GPIO.output(16, GPIO.LOW)  # Turn motor off
 
 
-if __name__ == '__main__':
-    try:
-        motor_on(channel)
-        time.sleep(1)
-        motor_off(channel)
-        time.sleep(1)
-        GPIO.cleanup()
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-
-                   
+#if __name__ == '__main__':
+#    try:
+#        rele1_on(channel)
+#        time.sleep(1)
+#        rele1_off(channel)
+#        time.sleep(1)
+#        GPIO.cleanup()
+#    except KeyboardInterrupt:
+#        GPIO.cleanup()
