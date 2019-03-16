209ec95859a6a0cec09f9d3aef4884eb20c9ffba
XGamingbg <48446414+XGamingbg@users.noreply.github.com>
2019-03-16 12:55:54 +0200

    2/3 ready

--- demo1.py
+++ demo1.py
@@ -3,16 +3,23 @@ import relay as rl
 import time
 import pompatarele as ml
 r = sr.Recognizer()
-while True:
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-        print(r.recognize_google(audio))
-        if(r.recognize_google(audio)=="make me coffee"):
-            rl.rele1_on(21)
-            time.sleep(10)#ne znaem vremeto
-            rl.rele1_off()
-            ml.rele2_on(20)
-            time.sleep(5)
-            ml.rele2_off()
+with sr.Microphone() as source:
+    while True:
+        try:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+            print(r.recognize_google(audio))
+            if(r.recognize_google(audio)=="make me coffee"):
+                rl.rele1_on(21)
+                time.sleep(10)#ne znaem vremeto
+                rl.rele1_off()
+                ml.rele2_on(20)
+                time.sleep(5)
+                ml.rele2_off()
+            elif(r.recognize_google(audio)=="stop"):
+                break
+            else:
+                print("Unknow Command")
+        except:
+            print("Unknow Command")
 
