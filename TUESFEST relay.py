bbbc3ac540f1d54ecdfc23e0ff26a30bf2af9df9
mrejariatroot <47807802+mrejariatroot@users.noreply.github.com>
2019-04-11 22:32:25 +0300

    Create TUESFEST relay.py

---
+++ TUESFEST relay.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+channel = 16
+
+# GPIO setup
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.OUT)
+
+
+def rele1_on():
+    GPIO.output(16, GPIO.HIGH)  # Turn motor on
+
+
+def rele1_off():
+    GPIO.output(16, GPIO.LOW)  # Turn motor off
+
+
+#if __name__ == '__main__':
+#    try:
+#        rele1_on(channel)
+#        time.sleep(1)
+#        rele1_off(channel)
+#        time.sleep(1)
+#        GPIO.cleanup()
+#    except KeyboardInterrupt:
+#        GPIO.cleanup()
+
