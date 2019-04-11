f9edb503b1d713d101b527afa8f2427f713b2fd1
XGamingbg <48446414+XGamingbg@users.noreply.github.com>
2019-04-11 18:49:31 +0300

    Update onwork.py
    Added functions to restart the program and listen forever. Tried to decrease the time of adjusting for ambient noise.

--- onwork.py
+++ onwork.py
@@ -10,20 +10,27 @@ ml.rele2_off()
 with sr.Microphone() as source:
     while True:
         try:
-            r.adjust_for_ambient_noise(source)
-            audio = r.listen(source)
-            text = r.recognize_google(audio)
-            print(text)
-            if("coffee" in text):
-                print("ok")
-                rl.rele1_on()
-                print(ldr.value)
-                while ldr.value < 1: #vinagi e po malko ot 1 toest vinagi proverqva dali sveti lampata
-                    if (ldr.value >0.80): #ako se e nagrqla chisloto e stoinostta na fotorezistora
-                        ml.rele2_on() #puskame pompata
-                        time.sleep(10) #deset sekundi kysoto kafe?!
-                        ml.rele2_off() #spirame pompata da ne se oleesh
+            def restart():
+                r.adjust_for_ambient_noise(source, duration=0.5)#Tova e za namalqvane na vremeto za slushane
+                audio = r.listen(source)
+                print("I'm listening") #Ako gornoto ne raboti pravilno tova shte e hint za koga usera da govori
+                text = r.recognize_google(audio)
+                print(text)
+                if("coffee" in text):
+                    print("ok")
+                    rl.rele1_on()
+                    print(ldr.value)
+                    while ldr.value < 1: #vinagi e po malko ot 1 toest vinagi proverqva dali sveti lampata
+                        if (ldr.value >0.80): #ako se e nagrqla chisloto e stoinostta na fotorezistora
+                            ml.rele2_on() #puskame pompata
+                            time.sleep(10) #deset sekundi kysoto kafe?!
+                            ml.rele2_off() #spirame pompata da ne se oleesh
+                            return restart1()
+            def restart1():
+                return restart()
             else:
                 print("Unknow Command") #ako neshto zburkash nali si ebe maikata :)
+                return restart()
         except:
             print("Unknow Command") #tva ne znam zashto e
+            return restart()
